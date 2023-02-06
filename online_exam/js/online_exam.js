39b2ac4702874fb757ae190348de7d15307aeb91
AudumabrKhare <audumbarkhare1112@gmail.com>
2023-02-06 20:39:21 +0530

    change the logic of question summary

--- online_exam/js/online_exam.js
+++ online_exam/js/online_exam.js
@@ -20,16 +20,21 @@ var que_marked = 0;
 var que_not_answered = 0;
 var que_not_visited = 0;
 
+var class_Name = "";
+
 var isQuestionAnswered = false;
 
 var questions_list = [];
 var dis_count = [];
+var selected_question_list = [];
 
 const question_details = [
     {
         id: 1,
         question: "what is the full form of IPL?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "A",
         option: [
             { A: "Indian Premier League", ans: 1 },
             { B: "Indian Player League", ans: 0 },
@@ -41,6 +46,8 @@ const question_details = [
         id: 2,
         question: "Which country has started IPL?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "A",
         option: [
             { A: "India", ans: 1 },
             { B: "South Africa", ans: 0 },
@@ -53,6 +60,8 @@ const question_details = [
         id: 3,
         question: "In which year did IPL start?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "C",
         option: [
             { A: "In 2005", ans: 0 },
             { B: "In 2007", ans: 0 },
@@ -64,6 +73,8 @@ const question_details = [
         id: 4,
         question: "How many teams are in the IPL right now?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "D",
         option: [
             { A: "7 Teams", ans: 0 },
             { B: "8 Teams", ans: 0 },
@@ -75,6 +86,8 @@ const question_details = [
         id: 5,
         question: "Which player was the first time to hit a six and a four in the IPL?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "C",
         option: [
             { A: "Chris Gayle", ans: 0 },
             { B: "Virat Kohli", ans: 0 },
@@ -86,6 +99,8 @@ const question_details = [
         id: 6,
         question: "Which player scored the first century in IPL?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "C",
         option: [
             { A: "Chris Gayle", ans: 0 },
             { B: "Virat Kohli", ans: 0 },
@@ -97,6 +112,8 @@ const question_details = [
         id: 7,
         question: "Which is the team that has won the most IPL trophy?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "B",
         option: [
             { A: "Chennai Super King", ans: 0 },
             { B: "Mumbai Indians", ans: 1 },
@@ -108,6 +125,8 @@ const question_details = [
         id: 8,
         question: "Which IPL team was banned for 2 years?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "A",
         option: [
             { A: "Chennai Super King or Rajasthan Royal", ans: 1 },
             { B: "New Super Kings or Royal Challenger Bangalore", ans: 0 },
@@ -119,6 +138,8 @@ const question_details = [
         id: 9,
         question: "Which batsman scored the most runs in the first season of IPL?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "C",
         option: [
             { A: " Virender Sehwag", ans: 0 },
             { B: "Shaun Marsh", ans: 0 },
@@ -130,6 +151,8 @@ const question_details = [
         id: 10,
         question: "Which player has been out at zero the most times in IPL?",
         answer: 1,
+        selected_ans: "",
+        correcte_ans: "B",
         option: [
             { A: "Harbhajan Singh", ans: 0 },
             { B: "Mandeep Singh", ans: 1 },
@@ -164,8 +187,8 @@ function show_question() {
                 }
             });
         }
-
-        flag == 1 ? i = i - 1 : questions_list.push(random_question_no);
+        flag == 1 ? i = i - 1 : selected_question_list.push(question_details[random_question_no]);
+        //flag == 1 ? i = i - 1 : questions_list.push(random_question_no);
     }
     // show_tips();
     display_questions();
@@ -185,9 +208,8 @@ function show_question() {
 // }
 
 function display_question_details() {
-    //showQuestion
     var show_question_list = "";
-    for (let i = 0; i < questions_list.length; i++) {
+    for (let i = 0; i < selected_question_list.length; i++) {
         show_question_list += `<li id="question-${i}">${i + 1}</li>`
     }
     showQuestionList.innerHTML = show_question_list;
@@ -199,53 +221,44 @@ function display_questions(type_action = '') {
     const totalQuestions = document.getElementById('total-questions');
     const questionList = document.getElementById(`question-${question_no}`);
 
-    //debugger;
-    if (type_action === "Next") {
-        console.log(question_no + " " + questions_list.length);
-        if (question_no < questions_list.length - 1) {
+    // console.log(questionList);
+
+    if (type_action == "Next") {
+        console.log((question_no < selected_question_list.length));
+        if (question_no < selected_question_list.length - 1) {
+            class_Name = questionList.className;
+            question_answered(class_Name, selected_question_list[question_no].selected_ans);
+
             questionList.classList.add('visited-question');
-            queNotVisited.innerText = --que_not_visited;
-        }
-
-        if (isQuestionAnswered) {
-            ++que_answered;
-            isQuestionAnswered = false;
-            queAnswered.innerText = que_answered;
-        } else {
-            ++que_not_answered;
-            queNotAnswered.innerText = que_not_answered;
-        }
-
-        question_no += 1;
-
-        if (question_no > questions_list.length - 1) {
-            alert('You are in last Question');
-            question_no -= 1;
-        }
-
-    } else if (type_action === "Prev") {
-        if (question_no != 0) questionList.classList.add('visited-question');
-
-        question_no -= 1;
-
-        if (question_no < 0) {
-            alert('You are in first Question');
+            if (class_Name != "visited-question") queNotVisited.innerText = --que_not_visited;
             question_no += 1;
+        } else {
+            alert('You are in last Question');
+        }
+
+    } else if (type_action == "Prev") {
+        if (question_no != 0) {
+            class_Name = questionList.className;
+            question_answered(class_Name, selected_question_list[question_no].selected_ans);
+            questionList.classList.add('visited-question');
+            if (class_Name != "visited-question") queNotVisited.innerText = --que_not_visited;
+            question_no -= 1;
+        } else {
+            alert('You are in first Question');
         }
     }
-    questionNo.innerText = question_no + 1;
-    totalQuestions.innerText = questions_list.length;
 
-    const result = question_details.find((item, index) => {
-        if (index == questions_list[question_no]) return item;
-    });
+    questionNo.innerText = question_no + 1;
+    totalQuestions.innerText = selected_question_list.length;
+
+    const result = selected_question_list[question_no];
 
     que = `<p id="question">${question_no + 1}.${result.question}</p>
                 <div class="show-option">
-                <label>A) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[0].A}" onclick="count_marks(${result.option[0].ans},${result.id});radio_disabled(${result.id})"/>&nbsp;${result.option[0].A}</label>
-                <label>B) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[1].B}" onclick="count_marks(${result.option[1].ans},${result.id});radio_disabled(${result.id})"/>&nbsp;${result.option[1].B}</label>
-                <label>C) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[2].c}" onclick="count_marks(${result.option[2].ans},${result.id});radio_disabled(${result.id})"/>&nbsp;${result.option[2].C}</label>
-               <label>D) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[3].D}" onclick="count_marks(${result.option[3].ans},${result.id});radio_disabled(${result.id})"/>&nbsp;${result.option[3].D}</label>
+                <label>A) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[0].A}" onclick="count_marks(${result.option[0].ans},${result.id});radio_disabled(${result.id},${question_no},'A')" ${result.selected_ans != '' ? 'disabled' : ''} ${result.selected_ans == 'A' ? 'checked' : ''}/>&nbsp;${result.option[0].A}</label>
+                <label>B) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[1].B}" onclick="count_marks(${result.option[1].ans},${result.id});radio_disabled(${result.id},${question_no},'B')" ${result.selected_ans != '' ? 'disabled' : ''} ${result.selected_ans == 'B' ? 'checked' : ''}/>&nbsp;${result.option[1].B}</label>
+                <label>C) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[2].c}" onclick="count_marks(${result.option[2].ans},${result.id});radio_disabled(${result.id},${question_no},'C')" ${result.selected_ans != '' ? 'disabled' : ''} ${result.selected_ans == 'C' ? 'checked' : ''}/>&nbsp;${result.option[2].C}</label>
+               <label>D) <input type="radio" name="option${result.id}" id="option${result.id}" value="${result.option[3].D}" onclick="count_marks(${result.option[3].ans},${result.id});radio_disabled(${result.id},${question_no},'D')" ${result.selected_ans != '' ? 'disabled' : ''} ${result.selected_ans == 'D' ? 'checked' : ''}/>&nbsp;${result.option[3].D}</label>
                </div>`;
     displayQuestions.innerHTML = que;
 }
@@ -273,8 +286,20 @@ function count_marks(marks, i) {
 }
 
 function show_result() {
+    var total_right_ans = 0;
+    var total_wrong_ans = 0;
+    selected_question_list.forEach((item, index) => {
+        if (item.selected_ans == item.correcte_ans) {
+            ++total_right_ans;
+        } else if (item.selected_ans != item.correcte_ans) {
+            ++total_wrong_ans;
+        }
+    });
+
+    console.log(total_right_ans);
+    total_marks = 5 * total_right_ans;
     console.log(total_marks);
-    reset();
+    //reset();
 }
 
 function reset() {
@@ -285,20 +310,30 @@ function reset() {
             radio[i].checked = false;
         });
     });
+
     min.innerHTML = 02;
     sec.innerHTML = 30;
     total_marks = 0;
     question_no = 0;
     que = '';
-    questions_list = [];
+    que_answered = 0;
+    que_marked = 0;
+    que_not_answered = 0;
+    que_not_visited = 0;
+
+    isQuestionAnswered = false;
+    selected_question_list = [];
     dis_count = [];
     show_question();
+
     document.getElementById("btn-result").disabled = true;
 }
 
-function radio_disabled(i) {
+function radio_disabled(i, index, selected_ans) {
     const radio = document.getElementsByName(`option${i}`);
-    // console.log(radio);
+
+    selected_question_list[index].selected_ans = selected_ans;
+
     radio.forEach((q, i) => {
         radio[i].disabled = true;
     });
@@ -326,8 +361,25 @@ function time_count() {
     }, 1000);
 
 }
+
+function question_answered(class_Name, selected_ans) {
+    console.log(class_Name + " " + isQuestionAnswered + " " + selected_ans);
+
+    if (isQuestionAnswered && selected_ans != "" && class_Name == "") {
+        ++que_answered;
+    } else if (!isQuestionAnswered && selected_ans == "" && class_Name == "") {
+        ++que_not_answered;
+    } else if (isQuestionAnswered && selected_ans != "" && class_Name == "visited-question") {
+        --que_not_answered;
+        ++que_answered;
+    }
+    queAnswered.innerText = que_answered;
+    queNotAnswered.innerText = que_not_answered;
+    isQuestionAnswered = false;
+}
+
 function display_summary() {
-    que_not_visited = questions_list.length;
+    que_not_visited = selected_question_list.length;
     queAnswered.innerText = que_answered;
     queMarked.innerText = que_marked;
     queNotAnswered.innerText = que_not_answered;
